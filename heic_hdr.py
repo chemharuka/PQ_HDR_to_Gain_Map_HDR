5592266c10d37126df4817dc4d391107d775e862
chemharuka <chemharuka@gmail.com>
2024-11-11 13:41:35 +0800

    set default PQHDRtoGMHDR dir

--- heic_hdr.py
+++ heic_hdr.py
@@ -27,20 +27,25 @@ if not tif_files:
     print(f"No {input_extension} files found in the directory {dir_path}.")
     sys.exit(1)
 
+# get working dir
+
+current_directory = os.getcwd()
+exc_path = os.path.join(current_directory, "PQHDRtoGMHDR")
+
 # define convert function
 def convert_to_heic(tif_file):
     input_file_path = os.path.join(dir_path, tif_file)
     output_file_path = dir_path
 
-    # run PQHDRtoGMHDR, please edit the path before running!
-    # run PQHDRtoGMHDR, please edit the path before running!
-    # run PQHDRtoGMHDR, please edit the path before running!
-
+    # run PQHDRtoGMHDR, please edit exc_path before running!
+    # sample: result = subprocess.run([‘/home/user/PQHDRtoGMHDR’, input ······
+    
     try:
-        result = subprocess.run(['./PQHDRtoGMHDR', input_file_path, output_file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run([exc_path, input_file_path, output_file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(f"Converted {tif_file} to {output_extension}")
     except subprocess.CalledProcessError as e:
         print(f"Error converting {tif_file}: {e.stderr.decode()}")
+
 
 # set max threads 8
 max_threads = 8
